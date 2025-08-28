335c780e68657c4a14e7557767e2ffd4cbb98774
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2025-08-28 20:54:29 +0300

    chore: feat add .env settings

---
+++ core/settings.py
@@ -0,0 +1,32 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
+
+
+class BaseServiceSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env', extra='ignore')
+
+
+class DBSettings(BaseServiceSettings):
+    user: str = Field(..., alias='DB_USER')
+    password: str = Field(..., alias='DB_PASSWORD')
+    host: str = Field(..., alias='DB_HOST')
+    port: int = Field(..., alias='DB_PORT')
+    name: str = Field(..., alias='DB_NAME')
+
+
+class UserbotSettings(BaseServiceSettings):
+    api_id: int = Field(..., alias='TELEGRAM_API_ID')
+    api_hash: str = Field(..., alias='TELEGRAM_API_HASH')
+
+
+class GigachatSettings(BaseServiceSettings):
+    api_key: str = Field(..., alias='GIGACHAT_API_KEY')
+
+
+class Settings(BaseServiceSettings):
+    db: DBSettings = DBSettings()
+    userbot: UserbotSettings = UserbotSettings()
+    gigachat: GigachatSettings = GigachatSettings()
+
+
+settins = Settings()
