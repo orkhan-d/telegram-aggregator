0f2593864548d486461f877fc2bd7b184a66ae1e
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2025-08-29 01:57:13 +0300

    fix: fix message model

--- src/models/message.py
+++ src/models/message.py
@@ -1,11 +1,14 @@
-from core.db import Base, UuidPkMixin, TimestampsMixin
+from core.db import Base, UuidPkMixin
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime as dt
+from sqlalchemy import BIGINT
 
 
-class Message(Base, UuidPkMixin, TimestampsMixin):
+class Message(Base, UuidPkMixin):
     __tablename__ = "messages"
 
     text: Mapped[str | None] = mapped_column()
     with_media: Mapped[bool] = mapped_column()
-    message_id: Mapped[int] = mapped_column()
-    channel_id: Mapped[int] = mapped_column()
+    message_id: Mapped[int] = mapped_column(type_=BIGINT)
+    channel_id: Mapped[int] = mapped_column(type_=BIGINT)
+    created_at: Mapped[dt] = mapped_column()
