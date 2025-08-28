18640b17faea2dd77e0cc841b9b3081879eba8af
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2025-08-28 21:40:21 +0300

    feat: init alembic

--- core/db.py
+++ core/db.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import MetaData
 from core.settings import settings
 
 
@@ -8,8 +9,20 @@ engine = create_async_engine(DATABASE_URL)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
 
+convention = {
+    'all_column_names': lambda constraint, table: '_'.join(
+        [column.name for column in constraint.columns.values()]
+    ),
+    'ix': 'ix__%(table_name)s__%(all_column_names)s',
+    'uq': 'uq__%(table_name)s__%(all_column_names)s',
+    'ck': 'ck__%(table_name)s__%(constraint_name)s',
+    'fk': 'fk__%(table_name)s__%(all_column_names)s__%(referred_table_name)s',
+    'pk': 'pk__%(table_name)s',
+}
+
+
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(naming_convention=convention)
 
 
 async def get_session() -> AsyncSession:
